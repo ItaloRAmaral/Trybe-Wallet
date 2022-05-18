0be84325d8b65617f7e13c08db1b849672ce3345
Italo Amaral <italorockamaral@gmail.com>
2022-05-18 15:07:50 -0300

    FEAT: Configurando Redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,3 +3,10 @@
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+import { combineReducers } from 'redux';
+import walletReducer from './wallet';
+import userReducer from './user';
+
+const rootReducer = combineReducers({ user: userReducer, wallet: walletReducer });
+
+export default rootReducer;
