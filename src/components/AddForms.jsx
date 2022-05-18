4fc3a79dc4483b63047545a80c060fc37c0af764
Italo Amaral <italorockamaral@gmail.com>
2022-05-18 19:24:31 -0300

    FEAT: Requisito 5 ok

--- src/components/AddForms.jsx
+++ src/components/AddForms.jsx
@@ -10,16 +10,64 @@ class AddForms extends React.Component {
   }
 
   render() {
+    const { currencies } = this.props;
     return (
       <section>
         <h1>Cadastro Despesas</h1>
+        <form>
+          <label htmlFor="despesa">
+            Valor
+            <input
+              type="text"
+              name="despesa"
+              id="despesa"
+              data-testid="value-input"
+            />
+          </label>
+          <label htmlFor="currency">
+            Moeda
+            <select id="currency">
+              {currencies.map((currency, index) => (
+                <option key={ index } value={ currency }>
+                  {currency}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label htmlFor="method">
+            Método de Pagamento
+            <select id="method" data-testid="method-input">
+              <option>Dinheiro</option>
+              <option>Cartão de débito</option>
+              <option>Cartão de crédito</option>
+            </select>
+          </label>
+          <label htmlFor="category">
+            Categoria
+            <select id="category" data-testid="tag-input">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+          <label htmlFor="description">
+            Descrição
+            <input
+              type="text"
+              id="description"
+              data-testid="description-input"
+            />
+          </label>
+        </form>
       </section>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  email: state.user.email,
+  currencies: state.wallet.currencies,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -28,6 +76,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 AddForms.propTypes = {
   getAllCurrencies: PropTypes.func.isRequired,
+  currencies: PropTypes.arrayOf.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddForms);
